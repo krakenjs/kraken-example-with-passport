a37d6aef8cfe6d7fd1a32bded879ce9226bb8289
Matt Edelman <medelman@paypal.com>
2014-04-02 15:29:21 -0400

    modify middleware event listener to act on middleware:after:session, removing extraneous console.log stmts

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
     var model = new AdminModel();
 
     app.get('/admin', auth.isAuthenticated('admin'), auth.injectUser(), function(req, res) {
-        //console.log("req.user", req.user);
         res.render('admin', model);
     });
 
