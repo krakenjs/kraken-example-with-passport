d2322f759b97fd30d006db632f30f7abe3719a16
Dennis Hall <dennis.hall@razorfish.com>
2016-02-15 13:27:23 -0500

    removes unused variable & duplicate function call

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
 var kraken = require('kraken-js'),
     app = require('express')(),
     options = require('./lib/spec')(app),
-    userLib = require('./lib/user')(),
     port = process.env.PORT || 8000;
-
 
 
 app.use(kraken(options));
